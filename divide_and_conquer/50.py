fb6be93c9768c600298d971760ff48731d63461b
l1n4n <nanli112358@gmail.com>
2018-11-24 19:34:44 +0800

    add recursive solution

--- divide_and_conquer/50.py
+++ divide_and_conquer/50.py
@@ -1,6 +1,8 @@
 # 50. Pow(x, n)
 # Implement pow(x, n), which calculates x raised to the power n.
 # IDEA: DIVIDE AND CONQUER
+
+# iterative
 class Solution(object):
     def myPow(self, x, n):
         """
@@ -17,3 +19,15 @@ class Solution(object):
             x *= x
             n >>= 1
         return res
+# recursive
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0: return 1
+        if n < 0: return 1/self.myPow(x, -n)
+        if n & 1: return x * self.myPow(x, n-1)
+        return self.myPow(x*x, n/2)
