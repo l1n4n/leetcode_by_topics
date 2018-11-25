ec464fc21dd4df35ae8f2ea9a17f4aa10cbf272d
l1n4n <nanli112358@gmail.com>
2018-11-25 13:01:21 +0800

    add another d&c solution similar with Moore Voting algorithm

--- divide_and_conquer/169.py
+++ divide_and_conquer/169.py
@@ -19,3 +19,25 @@ class Solution:
         if lm == rm: return lm
         # mid+1, right+1 !!!!!
         return lm if nums[left: mid+1].count(lm) > nums[mid+1: right+1].count(rm) else rm
+
+# A better d&c solution https://leetcode.com/problems/majority-element/discuss/177183/C++-Divide-and-conquer-O(N)-solution-(4-ms-beats-100)
+# similar with Moore Voting Algorithm
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 1: return nums[0]
+        # when n is odd, the last number will not be paired up
+        if n % 2:
+            if nums.count(nums[-1]) > n//2:
+                return nums[-1]
+        # pair up and keep the candidates --> cut (at least) size in half
+        temp = []
+        for i in range(0, n//2):
+            if nums[2*i] == nums[2*i+1]:
+                temp.append(nums[2*i])
+        return self.majorityElement(temp)
+            
