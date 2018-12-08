946a960782ecd4420c5d0c728c8a86c679346712
l1n4n <nanli112358@gmail.com>
2018-12-08 14:46:23 +0800

    add python sol for sqrt

---
+++ binary_search/69.py
@@ -0,0 +1,39 @@
+# 69. Sqrt(x)
+# Implement int sqrt(int x).
+
+# Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+# Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+# BS
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x <= 1: return x
+        left, right, mid = 1, x, 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            #if mid * mid == x:
+            if mid == x / mid: # to avoid overflow
+                return mid
+            elif mid < x / mid:
+                left = mid + 1
+                res = mid # keep the lower int bound in case x's sqrt is not an integer
+            else:
+                right = mid - 1
+        return res
+# Newton
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x <= 1: return x
+        guess = x
+        while guess > x / guess:
+            guess = (guess + x / guess) // 2
+        return int(guess)        
---
+++ binary_search/bs_temp.py
@@ -0,0 +1,12 @@
+# A binary search template
+"""
+left, right = 0, len(array) - 1
+while left <= right:
+	mid = left + (right - left) // 2
+	if array[mid] == target:
+		break or return result
+	elif array[mid] < target:
+		left = mid + 1
+	else:
+		right = mid - 1
+"""
